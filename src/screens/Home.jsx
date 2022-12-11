f148d6dce3e8443dc45d3975f9f5d109e2866fe3
maheshmuttinti <maheshmuttinti@betalectic.com>
2022-12-11 20:51:45 +0530

    add padding to root element

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -13,8 +13,10 @@ export default function Home() {
     <div className="mt-20 py-7 container mx-auto">
       <h1 className="text-2xl">Welcome to Favorite NPM Packages</h1>
       <div className="mt-24">
-        <div className="flex flex-col items-center justify-center border-2 border-slate-400 h-72">
-          <h2>You don't have any favs yet. Please add.</h2>
+        <div className="flex flex-col items-center justify-center border-2 border-slate-400 h-72 p-2">
+          <h2 className="text-center">
+            You don't have any favs yet. Please add.
+          </h2>
 
           <Button
             title="Add Fav"
